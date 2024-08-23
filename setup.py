917fe86fabb26f2645fb771ecaae839a94d01dac
Ashish Ghoshal <56021627+Ashish-Ghoshal@users.noreply.github.com>
2024-08-23 12:21:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
-from setuptools import setup, find_packages
-from typing import List
+
+
 import logging
+import setuptools
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger("SetupLogger")
 
 # Project metadata
-PROJ_TITLE = "Face Authenticator"  
-VER_NUM = "0.0.1"
-AUTHOR_NAME = "Ashish Ghoshal"
-PROJECT_DESC = "This is a Face Authenticator Project"
+TITLE = "Facial Verification"  
+NUM = "0.1.5"
+NAME = "Ashish Ghoshal"
+DESC = " Facial Verfication Project"
 
 
 # Setting up the package with the above information
 
-setup(
-    name=PROJ_TITLE,
-    version=VER_NUM,
-    author=AUTHOR_NAME,
-    description=PROJECT_DESC,
-    packages=find_packages(),
+setuptools.setup(
+    name=TITLE,
+    version=NUM,
+    author=NAME,
+    description=DESC,
+    packages=setuptools.find_packages(),
 )
 
 log.info("Setup script executed successfully.")
