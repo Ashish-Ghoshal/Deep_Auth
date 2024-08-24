8fbf40ef3a7e91215525f0c7150bdbcb75aecc54
Ashish-Ghoshal <ghoshal.ashish29@gmail.com>
2024-08-24 21:30:41 +0530

    Redo

--- auth__logic/constant/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-from datetime import datetime
-
-from auth__logic.constant.database_constants import *
-
-PIPELINE_NAME = "face"
-PIPELINE_ARTIFACT_DIR = os.path.join(os.getcwd(), "finance_artifact")
-
-TIMESTAMP = datetime.now().strftime("%Y%m%d_%H%M%S")
--- auth__logic/constant/auth_constant.py
+++
@@ -1,5 +0,0 @@
-from auth__logic.utils.util import CommonUtils
-
-
-SECRET_KEY = CommonUtils().get_environment_variable("SECRET_KEY")
-ALGORITHM = CommonUtils().get_environment_variable("ALGORITHM")
